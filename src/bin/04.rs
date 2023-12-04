a54c635f2cb3cbec558e56028984f1e1ef9b6d38
Mike Repec <mikerepec@gmail.com>
2023-12-04 22:35:54 +0100

    Day 4 Part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -8,19 +10,39 @@ pub fn part_one(input: &str) -> Option<u32> {
                 let (raw_wins, raw_picks) =
                     l.split_once(": ").unwrap().1.split_once(" | ").unwrap();
                 let (wins, picks) = (parse_nums(raw_wins), parse_nums(raw_picks));
-                let won_amount = picks.iter().filter(|n| wins.contains(n)).count();
+                let count = picks.iter().filter(|n| wins.contains(n)).count() as u32;
 
-                match won_amount {
+                match count {
                     0 => 0,
-                    _ => 2_u32.pow((won_amount - 1).try_into().unwrap()),
+                    _ => 2_u32.pow(count - 1),
                 }
             })
             .sum(),
     )
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut copies: HashMap<usize, u32> = Default::default();
+
+    Some(
+        input
+            .lines()
+            .enumerate()
+            .map(|(i, l)| {
+                let (raw_wins, raw_picks) =
+                    l.split_once(": ").unwrap().1.split_once(" | ").unwrap();
+                let (wins, picks) = (parse_nums(raw_wins), parse_nums(raw_picks));
+                let won_amount = picks.iter().filter(|n| wins.contains(n)).count();
+
+                let copies_amount = *copies.get(&i).unwrap_or(&0);
+                for j in (i + 1)..(i + 1 + won_amount) {
+                    *(copies.entry(j).or_default()) += 1 + copies_amount;
+                }
+
+                copies_amount + 1
+            })
+            .sum(),
+    )
 }
 
 fn parse_nums(raw: &str) -> Vec<u32> {
@@ -33,13 +55,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(13));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(30));
     }
 }
