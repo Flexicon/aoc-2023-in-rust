3078d326407591c0f2ca0b1f5098a076c0bda181
Mike Repec <mikerepec@gmail.com>
2023-12-06 17:58:22 +0100

    Day 6 Part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -16,8 +16,19 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let (line1, line2) = input.split_once("\n").unwrap();
+    let (time, record) = (parse_as_whole_num(line1), parse_as_whole_num(line2));
+
+    Some((1..time - 1).filter(|i| i * (time - i) > record).count() as u32)
+}
+fn parse_as_whole_num(raw: &str) -> u64 {
+    raw.replace(" ", "")
+        .split_once(":")
+        .unwrap()
+        .1
+        .parse()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -33,6 +44,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71503));
     }
 }
