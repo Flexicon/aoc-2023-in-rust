dad59dea41b9a4e2509efa2a3293b47681fae3da
Mike Repec <mikerepec@gmail.com>
2023-12-07 23:02:03 +0100

    Day 7 Part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -5,6 +5,7 @@ use itertools::Itertools;
 advent_of_code::solution!(7);
 
 const LABELS: &str = "23456789TJQKA";
+const LABELS_P2: &str = "J23456789TQKA";
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum HandKind {
@@ -22,6 +23,17 @@ struct Hand {
     kind: HandKind,
     value: String,
     bid: u32,
+    part: u8,
+}
+
+impl Hand {
+    fn labels(&self) -> &str {
+        if self.part == 1 {
+            LABELS
+        } else {
+            LABELS_P2
+        }
+    }
 }
 
 impl Ord for Hand {
@@ -29,7 +41,7 @@ impl Ord for Hand {
         match self.kind.cmp(&other.kind) {
             Ordering::Equal => {
                 for (a, b) in self.value.chars().zip(other.value.chars()) {
-                    match cmp_labels(a, b) {
+                    match cmp_labels(self.labels(), a, b) {
                         Ordering::Equal => continue,
                         ord => return ord,
                     }
@@ -65,6 +77,7 @@ pub fn part_one(input: &str) -> Option<u32> {
                     kind: parse_kind(hand),
                     value: String::from(hand),
                     bid: bid.parse().unwrap(),
+                    part: 1,
                 }
             })
             .sorted()
@@ -74,12 +87,30 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        input
+            .split("\n")
+            .filter(|l| !l.trim().is_empty())
+            .map(|line| {
+                let (hand, bid) = line.split_once(" ").unwrap();
+
+                Hand {
+                    kind: parse_kind_p2(hand),
+                    value: String::from(hand),
+                    bid: bid.parse().unwrap(),
+                    part: 2,
+                }
+            })
+            .sorted()
+            .enumerate()
+            .map(|(i, h)| h.bid * (i as u32 + 1))
+            .sum(),
+    )
 }
 
-fn cmp_labels(a: char, b: char) -> Ordering {
-    LABELS.find(a).unwrap().cmp(&LABELS.find(b).unwrap())
+fn cmp_labels(lbls: &str, a: char, b: char) -> Ordering {
+    lbls.find(a).unwrap().cmp(&lbls.find(b).unwrap())
 }
 
 fn parse_kind(hand: &str) -> HandKind {
@@ -95,17 +126,64 @@ fn parse_kind(hand: &str) -> HandKind {
             }
         })
         .filter_map(|(_, n)| if n > 1 { Some(n.to_string()) } else { None })
+        .sorted()
         .collect::<Vec<String>>()
         .join("");
 
-    match repeats.as_str() {
+    repeat_str_as_kind(repeats.as_str())
+}
+
+fn parse_kind_p2(hand: &str) -> HandKind {
+    let mut jokers = 0;
+    let mut pairs = hand
+        .chars()
+        .sorted()
+        .filter_map(|c| {
+            if c == 'J' {
+                jokers += 1;
+                None
+            } else {
+                Some((c, 1))
+            }
+        })
+        .coalesce(|(a, n), (b, m)| {
+            if a == b {
+                Ok((a, n + m))
+            } else {
+                Err(((a, n), (b, m)))
+            }
+        })
+        .map(|(_, n)| n)
+        .sorted()
+        .collect::<Vec<i32>>();
+
+    if pairs.len() == 0 {
+        pairs = Vec::from([jokers]);
+    } else {
+        let highest_pair = pairs.last_mut().unwrap();
+        *highest_pair += jokers;
+    }
+
+    let repeats = pairs
+        .iter()
+        .filter(|v| v > &&1)
+        .map(|v| v.to_string())
+        .collect::<Vec<String>>()
+        .join("");
+
+    repeat_str_as_kind(repeats.as_str())
+}
+
+fn repeat_str_as_kind(repeats: &str) -> HandKind {
+    match repeats {
         "2" => HandKind::OnePair,
         "3" => HandKind::ThreeOfAKind,
         "4" => HandKind::FourOfAKind,
         "5" => HandKind::FiveOfAKind,
-        "23" | "32" => HandKind::FullHouse,
+        "23" => HandKind::FullHouse,
         "22" => HandKind::TwoPair,
-        _ => HandKind::HighCard,
+        "" => HandKind::HighCard,
+        v => panic!("Unexpected repeated cards value: '{v}'"),
     }
 }
 
@@ -128,6 +206,12 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5905));
+    }
+
+    #[test]
+    fn test_part_two_answer() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(249817836));
     }
 }
