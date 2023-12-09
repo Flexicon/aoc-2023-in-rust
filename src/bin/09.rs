6d1108e28c86b55bcb857b920753eda02fa7bb5f
Mike Repec <mikerepec@gmail.com>
2023-12-09 19:10:23 +0100

    Day 9 Part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,7 +1,26 @@
 advent_of_code::solution!(9);
 
 pub fn part_one(input: &str) -> Option<i32> {
-    let sequences = input
+    Some(solve(input, predict_p1))
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    Some(solve(input, predict_p2))
+}
+
+fn solve(input: &str, predict: fn(&Vec<i32>) -> i32) -> i32 {
+    let sequences = parse_sequences(input);
+    let mut predictions: Vec<i32> = Default::default();
+
+    for seq in sequences {
+        predictions.push(predict(&seq));
+    }
+
+    predictions.iter().sum()
+}
+
+fn parse_sequences(input: &str) -> Vec<Vec<i32>> {
+    input
         .split("\n")
         .filter(|l| !l.trim().is_empty())
         .map(|l| {
@@ -9,40 +28,46 @@ pub fn part_one(input: &str) -> Option<i32> {
                 .map(|v| v.parse().unwrap())
                 .collect::<Vec<i32>>()
         })
-        .collect::<Vec<_>>();
-
-    let mut predictions: Vec<i32> = Default::default();
-
-    for seq in sequences {
-        let mut lasts: Vec<i32> = Vec::from([*seq.last().unwrap()]);
-        let mut cur = seq.clone();
-
-        while !all_zeroes(&cur) {
-            let next = cur
-                .iter()
-                .enumerate()
-                .filter_map(|(i, n)| {
-                    if i < cur.len() - 1 {
-                        let next_value = cur[i + 1];
-                        Some(next_value - n)
-                    } else {
-                        None
-                    }
-                })
-                .collect::<Vec<_>>();
-
-            lasts.push(*next.last().unwrap());
-            cur = next.clone();
-        }
-
-        predictions.push(lasts.iter().sum());
-    }
-
-    Some(predictions.iter().sum())
+        .collect::<Vec<_>>()
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+fn predict_p1(seq: &Vec<i32>) -> i32 {
+    let (_, lasts) = firsts_and_lasts(&seq);
+    lasts.iter().sum()
+}
+
+fn predict_p2(seq: &Vec<i32>) -> i32 {
+    let mut firsts = firsts_and_lasts(&seq).0.clone();
+    firsts.reverse();
+
+    firsts.iter().skip(1).fold(firsts[0], |acc, n| n - acc)
+}
+
+fn firsts_and_lasts(seq: &Vec<i32>) -> (Vec<i32>, Vec<i32>) {
+    let mut firsts: Vec<i32> = Vec::from([*seq.first().unwrap()]);
+    let mut lasts: Vec<i32> = Vec::from([*seq.last().unwrap()]);
+    let mut cur = seq.clone();
+
+    while !all_zeroes(&cur) {
+        let next = cur
+            .iter()
+            .enumerate()
+            .filter_map(|(i, n)| {
+                if i < cur.len() - 1 {
+                    let next_value = cur[i + 1];
+                    Some(next_value - n)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+
+        firsts.push(*next.first().unwrap());
+        lasts.push(*next.last().unwrap());
+        cur = next.clone();
+    }
+
+    (firsts, lasts)
 }
 
 fn all_zeroes(seq: &Vec<i32>) -> bool {
@@ -62,6 +87,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
