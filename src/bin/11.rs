9a8110c8fd9c38676ee3f13ed18916c941d0cfb7
Mike Repec <mikerepec@gmail.com>
2023-12-11 19:54:58 +0100

    Clean up Day 11 part 1 a bit

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -3,44 +3,14 @@ use itertools::{enumerate, Itertools};
 advent_of_code::solution!(11);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let grid = input
-        .split('\n')
-        .filter(|l| !l.is_empty())
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    let mut empty_cols: Vec<usize> = Default::default();
-    let mut empty_rows: Vec<usize> = Default::default();
-
-    for (i, _) in enumerate(&grid[0]) {
-        let col_empty = !grid.iter().map(|row| row[i]).contains(&'#');
-
-        if col_empty {
-            empty_cols.push(i)
-        }
-
-        if grid[i].iter().all(|c| c == &'.') {
-            empty_rows.push(i)
-        }
-    }
-
-    let mut galaxies: Vec<(usize, usize)> = Default::default();
-    for (i, row) in enumerate(&grid) {
-        for (j, col) in enumerate(row) {
-            if col == &'#' {
-                galaxies.push((i, j))
-            }
-        }
-    }
+    let grid = parse_grid(input);
+    let (empty_rows, empty_cols) = find_empty_rows_and_cols(&grid);
+    let galaxies = find_galaxies(&grid);
 
     let mut distances: Vec<u32> = Default::default();
 
     for (i, g1) in enumerate(&galaxies) {
         for g2 in galaxies.iter().skip(i) {
-            if g1 == g2 {
-                continue;
-            }
-
             let extra_cols = empty_cols
                 .iter()
                 .filter(|v| (g1.1..=g2.1).contains(v) || (g2.1..=g1.1).contains(v))
@@ -60,6 +30,47 @@ pub fn part_one(input: &str) -> Option<u32> {
 
 pub fn part_two(_: &str) -> Option<u32> {
     None
+}
+
+fn find_galaxies(grid: &[Vec<char>]) -> Vec<(usize, usize)> {
+    let mut galaxies: Vec<(usize, usize)> = Default::default();
+
+    for (i, row) in enumerate(grid) {
+        for (j, col) in enumerate(row) {
+            if col == &'#' {
+                galaxies.push((i, j))
+            }
+        }
+    }
+
+    galaxies
+}
+
+fn find_empty_rows_and_cols(grid: &[Vec<char>]) -> (Vec<usize>, Vec<usize>) {
+    let mut empty_cols: Vec<usize> = Default::default();
+    let mut empty_rows: Vec<usize> = Default::default();
+
+    for (i, _) in enumerate(&grid[0]) {
+        let col_empty = !grid.iter().map(|row| row[i]).contains(&'#');
+
+        if col_empty {
+            empty_cols.push(i)
+        }
+
+        if grid[i].iter().all(|c| c == &'.') {
+            empty_rows.push(i)
+        }
+    }
+
+    (empty_rows, empty_cols)
+}
+
+fn parse_grid(input: &str) -> Vec<Vec<char>> {
+    input
+        .split('\n')
+        .filter(|l| !l.is_empty())
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>()
 }
 
 #[cfg(test)]
