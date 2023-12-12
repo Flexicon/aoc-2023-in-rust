079a15ec411e1c3a9a488b3a36c5f72572d4ddd1
Mike Repec <mikerepec@gmail.com>
2023-12-12 23:07:21 +0100

    Day 12 Part 2

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,23 +1,30 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(12);
 
 pub fn part_one(input: &str) -> Option<i64> {
-    let lines = parse_input(input);
-    let mut sum = 0;
-
-    for (springs, groups) in &lines {
-        sum += arrangement_count(0, 0, springs, groups);
-    }
-
-    Some(sum)
+    solve(input, true)
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    solve(input, false)
 }
 
-fn parse_input(input: &str) -> Vec<(String, Vec<usize>)> {
+fn solve(input: &str, is_part_1: bool) -> Option<i64> {
+    Some(
+        parse_input(input, is_part_1)
+            .iter()
+            .map(|(springs, groups)| {
+                let mut memo: HashMap<(usize, usize), i64> = Default::default();
+                arrangement_count(0, 0, springs, groups, &mut memo)
+            })
+            .sum(),
+    )
+}
+
+fn parse_input(input: &str, part1: bool) -> Vec<(String, Vec<usize>)> {
     input
         .split('\n')
         .filter(|l| !l.is_empty())
@@ -28,12 +35,36 @@ fn parse_input(input: &str) -> Vec<(String, Vec<usize>)> {
                 .map(|v| v.parse().unwrap())
                 .collect::<Vec<usize>>();
 
-            (format!("{}.", springs), groups)
+            if part1 {
+                (format!("{}.", springs), groups)
+            } else {
+                let s_repeat = format!("{}?", springs).repeat(5);
+                let expanded_springs = &s_repeat[..s_repeat.len() - 1];
+                let exapnded_groups = groups
+                    .iter()
+                    .cycle()
+                    .take(groups.len() * 5)
+                    .copied()
+                    .collect_vec();
+
+                (format!("{}.", expanded_springs), exapnded_groups)
+            }
         })
         .collect_vec()
 }
 
-fn arrangement_count(p: usize, g: usize, springs: &String, groups: &[usize]) -> i64 {
+fn arrangement_count(
+    p: usize,
+    g: usize,
+    springs: &String,
+    groups: &[usize],
+    memo: &mut HashMap<(usize, usize), i64>,
+) -> i64 {
+    // Check memo cache first - 🏎️
+    if let Some(cached) = memo.get(&(p, g)) {
+        return *cached;
+    }
+
     // No more groups
     if g >= groups.len() {
         // But there are still springs left in the row - not a solution
@@ -57,28 +88,31 @@ fn arrangement_count(p: usize, g: usize, springs: &String, groups: &[usize]) -> 
         .nth(p + group_size)
         .unwrap_or(springs.chars().last().unwrap());
 
-    match cur_spring {
+    let count = match cur_spring {
         '?' => {
             if !springs[p..group_end].contains('.') && group_neighbor != '#' {
-                arrangement_count(p + group_size + 1, g + 1, springs, groups)
-                    + arrangement_count(p + 1, g, springs, groups)
+                arrangement_count(p + group_size + 1, g + 1, springs, groups, memo)
+                    + arrangement_count(p + 1, g, springs, groups, memo)
             } else {
                 // Working spring, move on to check the next one
-                arrangement_count(p + 1, g, springs, groups)
+                arrangement_count(p + 1, g, springs, groups, memo)
             }
         }
         '#' => {
             // Check if damaged group can start here, from this existing damaged spring
             if !springs[p..group_end].contains('.') && group_neighbor != '#' {
-                arrangement_count(p + group_size + 1, g + 1, springs, groups)
+                arrangement_count(p + group_size + 1, g + 1, springs, groups, memo)
             } else {
                 0 // Not a solution
             }
         }
         // Working spring, move on to check the next one
-        '.' => arrangement_count(p + 1, g, springs, groups),
+        '.' => arrangement_count(p + 1, g, springs, groups, memo),
         _ => unreachable!("Should never happen!"),
-    }
+    };
+
+    memo.insert((p, g), count);
+    count
 }
 
 #[cfg(test)]
@@ -94,6 +128,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(525152));
     }
 }
