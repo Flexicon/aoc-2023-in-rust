8b9cd6922d76d9e14e609ff6b9efc138d1f13be7
Mike Repec <mikerepec@gmail.com>
2023-12-16 12:13:47 +0100

    Day 15 Part 2

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,20 +1,57 @@
+use itertools::{enumerate, Itertools};
+
 advent_of_code::solution!(15);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let sum = input
-        .split(',')
-        .map(|s| {
-            s.chars()
-                .filter(|c| c != &'\n')
-                .fold(0, |sum, c| ((sum + c as u32) * 17) % 256)
-        })
-        .sum();
+    let sum = input.trim().split(',').map(hash_seq).sum();
 
     Some(sum)
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut boxes: Vec<Vec<(&str, u32)>> = vec![vec![]; 256];
+
+    // Calculate steps
+    let steps = input
+        .trim()
+        .split(',')
+        .map(|s| {
+            let (seq, len) = s.split_once(|c| matches!(c, '=' | '-')).unwrap();
+            (hash_seq(seq) as usize, seq, len.parse::<u32>().ok())
+        })
+        .collect::<Vec<_>>();
+
+    // Perform steps, filling boxes as appropriate
+    for (box_num, label, focal) in steps {
+        let b = boxes.get_mut(box_num).unwrap();
+        let found = b.iter().find_position(|(s, _)| s == &label);
+
+        match found {
+            Some((i, _)) => {
+                b.remove(i);
+                if let Some(focal) = focal {
+                    b.insert(i, (label, focal));
+                }
+            }
+            None => {
+                if let Some(focal) = focal {
+                    b.push((label, focal))
+                }
+            }
+        }
+    }
+
+    let mut sum = 0;
+    for (i, b) in enumerate(boxes) {
+        for (j, (_, focal)) in enumerate(b) {
+            sum += (i as u32 + 1) * (j as u32 + 1) * focal;
+        }
+    }
+    Some(sum)
+}
+
+fn hash_seq(seq: &str) -> u32 {
+    seq.chars().fold(0, |sum, c| ((sum + c as u32) * 17) % 256)
 }
 
 #[cfg(test)]
@@ -30,6 +67,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(145));
     }
 }
